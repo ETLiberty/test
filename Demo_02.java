01f55caead52a4a1ad3f124a6a8eb66af4e2a31b
ETLiberty <124643287+ETLiberty@users.noreply.github.com>
2023-02-07 10:31:54 +0800

    第二次添加
    .

---
+++ Demo_02.java
@@ -0,0 +1,10 @@
+package OOP.day01;
+
+public class Demo_02 {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        int r = 2;
+        double area = PI*r*r;
+        System.out.println(area);
+    }
+}
