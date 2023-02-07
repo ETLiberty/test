d813aea9617e74ad183e4f0f953d2bb0fb73dd52
ETLiberty <124643287+ETLiberty@users.noreply.github.com>
2023-02-07 10:27:23 +0800

    Add files via upload
    添加更新

---
+++ Demo_01.java
@@ -0,0 +1,12 @@
+package OOP.day01;
+
+public class Demo_01 {
+    public static void main(String[] args) {
+    double score = 98.5;
+    String name ="张三";
+    char sex ='男';
+        System.out.println("最高分："+score);
+        System.out.println("最高分学员名字："+name);
+        System.out.println("最高分学员性别："+sex);
+}
+}
